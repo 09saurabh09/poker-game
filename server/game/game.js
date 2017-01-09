bc5b9b2b4248ab7c9a407f663f9cb2283d2b95ce
Devil29 <vishalraj48@gmail.com>
2017-01-09 15:48:28 +0530

    Fixed the sorting algorithm for the cards

--- server/game/game.js
+++ server/game/game.js
@@ -1,3 +1,7 @@
+/**
+ * Created by Vishal Kumar
+ */
+
 "use strict";
 module.exports = Game;
 
--- server/utils/evaluator.js
+++ server/utils/evaluator.js
@@ -8,16 +8,18 @@ module.exports = Evaluator;
 Evaluator.prototype.sortByRank = function(hands, players){
 	var evalHands = [];
 
-    for (i=0; i<hands.length; i++) {
+    for (i = 0; i < hands.length ; i++ ) {
     	var playerHand = {};
     	playerHand.player = players[i];
-    	playerHand.hand = PokerEvaluator.evalHand(hands[i])
+    	playerHand.hand = PokerEvaluator.evalHand(hands[i]);
         evalHands.push(playerHand);
     }
 
     evalHands.sort(function(a,b){
-		if(a.playerHand.val > b.playerHand.val)
-			return 0;
+		if(a.hand > b.hand)
+			return -1;
+        else if (a.hand == b.hand)
+            return 0;
 		return 1;
 	});
 
