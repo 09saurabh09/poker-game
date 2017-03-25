00c13961c4fee1f60a03ede73ad123d4078f0c0f
Saurabh Kumar <saurabhk@olacabs.com>
2017-03-25 16:08:26 +0530

    moment time fixed

--- server/utils/timer.js
+++ server/utils/timer.js
@@ -1,6 +1,7 @@
 "use strict";
 
-let moment = require('moment-timer');
+let moment = require('moment');
+let momentTimer = require('../utils/momentTimer');
 let pokerTableConfig = require('../game/pokerTableConfig');
 
 module.exports = {
@@ -9,7 +10,7 @@ module.exports = {
         // let pos = game.findPlayerPos(user.id);
         let playerTimeBank = user.timeBank;
 
-        GlobalConstant.timers[game.tableId] = moment.duration(pokerTableConfig.timer.defaultDuration + playerTimeBank, "seconds").timer(function () {
+        GlobalConstant.timers[game.tableId] = momentTimer.timer(moment.duration(pokerTableConfig.timer.defaultDuration + playerTimeBank, "seconds"),function () {
             console.log(`INFO ::: Started timer for player ${user.id}`);
             let turnType = "timer";
             self.playerTurn({ user, game, turnType})
