1c93ddefba23b5bf9d23414d768c56d152ed1b12
Devil29 <vishalraj48@gmail.com>
2017-03-25 14:27:00 +0530

    Case to handle when the raise or call is equal to his stack

--- server/game/player.js
+++ server/game/player.js
@@ -92,16 +92,20 @@ Player.prototype.callOrCheck = function() {
     this.hasActed = true;
 
     var diff = this.game.getHighestBet() - this.bet;
-    this.addBet(diff);
 
-    if (diff > 0) {
-        this.lastAction = "call";
-        logd('Player ' + this.name + ' CALL : ' + diff);
+    if(diff >= this.chips){
+        this.allin();
     } else {
-        this.lastAction = "check";
-        logd('Player ' + this.name + ' CHECK');
+        this.addBet(diff);
+        if (diff > 0) {
+            this.lastAction = "call";
+            logd('Player ' + this.name + ' CALL : ' + diff);
+        } else {
+            this.lastAction = "check";
+            logd('Player ' + this.name + ' CHECK');
+        }
+        this.moveNext();
     }
-    this.moveNext();
 };
 
 
@@ -140,14 +144,17 @@ Player.prototype.doBestCall = function(){
 Player.prototype.raise = function(amount) {
     this.lastAction = "raise";
 
-    var diff = this.game.getHighestBet() - this.bet;
-    this.addBet(diff + amount);
+    var diff = amount - this.bet;
 
-    logd('Player ' + this.name + ' Raises : ' + (diff + amount));
-
-    this.game.requestPlayerAction(); // other players must act
-    this.hasActed = true;
-    this.moveNext();
+    if(diff >= this.chips){
+        this.allin();
+    } else {
+        this.addBet(diff);
+        logd('Player ' + this.name + ' Raises To : ' + amount + "raise Amount" + diff );
+        this.game.requestPlayerAction(); // other players must act
+        this.hasActed = true;
+        this.moveNext();
+    }
 };
 
 
